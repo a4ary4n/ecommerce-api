1e1075df57904fbc59c37bd2ccaf61b96b10247c
Aryan Surana <surana.aryan88@gmail.com>
2026-07-04 20:14:42 +0530

    initial Spring Boot project setup

---
+++ src/main/kotlin/dev/aryan/ecommerceapi/EcommerceApiApplication.kt
@@ -0,0 +1,11 @@
+package dev.aryan.ecommerceapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EcommerceApiApplication
+
+fun main(args: Array<String>) {
+    runApplication<EcommerceApiApplication>(*args)
+}
---
+++ src/test/kotlin/dev/aryan/ecommerceapi/EcommerceApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package dev.aryan.ecommerceapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EcommerceApiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
